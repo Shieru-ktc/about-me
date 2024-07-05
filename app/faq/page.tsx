f3ff3cbd0e03e0f5e750e869d097dd49e2f277bb
Shieru-ktc <167487026+Shieru-ktc@users.noreply.github.com>
2024-07-05 14:25:30 +0900

    tesuto
    ganbatta yo

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -4,58 +4,76 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+
+const FAQS = [
+  {
+    question: "なんの言語が好き？",
+    answer: "プログラミング言語で言えば、Pythonがだいすき。でもこのサイトはTypeScriptで書いてる。うーんこの。",
+  },
+  {
+    question: "Android派？iPhone派？",
+    answer: (
+      <>
+        Androidしか勝たん。iPhone使うとPC、イヤホン、スマートウォッチやらなんやらApple製品で固めないといけないのがつらいんだよね。
+        <br />
+        いずれApple Houseとか言って家出してきそう。あとApple CarとかApple
+        葬式とか。
+      </>
+    ),
+  },
+  {
+    question: "好きなコードについて",
+    answer: (
+      <>
+        <code>0xfor-d*University</code>
+        <br />
+        これはPythonのコードです。なにが出力されるか予想してみてください。
+        <br />
+        エラー？それとも、何らかの別の結果？
+      </>
+    ),
+  },
+  {
+    question: "好きなPhasmophobiaのゴーストは？",
+    answer: "ハントゥちゃんわかりやすくてすき。",
+  },
+  {
+    question: "好きなBeat Saberの譜面は？",
+    answer: (
+      <>
+        Spin Eternallyとか、
+        <a
+          href="https://beatsaver.com/maps/e4d"
+          className="text-blue-600 no-underline hover:underline dark:text-blue-400"
+        >
+          Ange du blanc pur
+        </a>
+        とか。
+      </>
+    ),
+  },
+];
+
+type FAQProps = {
+  i: number;
+  question: React.ReactNode;
+  answer: React.ReactNode;
+}
+const FAQComponent: React.FC<FAQProps> = ({ i, question, answer }) => {
+  return (
+    <AccordionItem value={`value-${i}`}>
+      <AccordionTrigger>{question}</AccordionTrigger>
+      <AccordionContent>{answer}</AccordionContent>
+    </AccordionItem>
+  )
+}
 const FAQPage: React.FC = () => {
   return (
     <>
       <Accordion type="multiple">
-        <AccordionItem value="item-3">
-          <AccordionTrigger>なんの言語が好き？</AccordionTrigger>
-          <AccordionContent>
-            プログラミング言語で言えば、Pythonがだいすき。でもこのサイトはTypeScriptで書いてる。うーんこの。
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-4">
-          <AccordionTrigger>Android派？iPhone派？</AccordionTrigger>
-          <AccordionContent>
-            <p>
-              Androidしか勝たん。iPhone使うとPC、イヤホン、スマートウォッチやらなんやらApple製品で固めないといけないのがつらいんだよね。
-              <br />
-              いずれApple Houseとか言って家出してきそう。あとApple CarとかApple
-              葬式とか。
-            </p>
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-5">
-          <AccordionTrigger>好きなコードについて</AccordionTrigger>
-          <AccordionContent>
-            <code>0xfor-d*University</code>
-            <br />
-            <p>
-              これはPythonのコードです。なにが出力されるか予想してみてください。
-              <br />
-              エラー？それとも、何らかの別の結果？
-            </p>
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-6">
-          <AccordionTrigger>好きなPhasmophobiaのゴーストは？</AccordionTrigger>
-          <AccordionContent>
-            ハントゥちゃんわかりやすくてすき。
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-7">
-          <AccordionTrigger>好きなBeat Saberの譜面は？</AccordionTrigger>
-          <AccordionContent>
-            Spin Eternallyとか、
-            <a
-              href="https://beatsaver.com/maps/e4d"
-              className="text-blue-600 no-underline hover:underline dark:text-blue-400"
-            >
-              Ange du blanc pur
-            </a>
-            とか。
-          </AccordionContent>
-        </AccordionItem>
+        {FAQS.map((faq, i) => (
+          <FAQComponent key={i} i={i} question={faq.question} answer={faq.answer} />
+        ))}
       </Accordion>
     </>
   );
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -25,25 +25,27 @@ const ProfilePage = () => {
       </dl>
       <hr />
       <h2 className="text-xl">学歴</h2>
-      <p>20xx年 米原市立坂田小学校 卒業</p>
-      <p>20xx年 米原市立双葉中学校 卒業</p>
-      <p>20xx年 角川ドワンゴ学園 S高等学校 卒業</p>
-      <p>20xx年 京都デザイン&テクノロジー専門学校 入学</p>
+      <p>20xx年 米原市立坂田小学校 卒業<small className="text-gray-500 ml-1">(教師がクソでした)</small></p>
+      <p>20xx年 米原市立双葉中学校 卒業<small className="text-gray-500 ml-1">(プログラミングがやりたすぎて不登校になりました)</small></p>
+      <p>20xx年 角川ドワンゴ学園 S高等学校 卒業<small className="text-gray-500 ml-1">(最近情報漏洩したとこです)</small></p>
+      <p>20xx年 京都デザイン&テクノロジー専門学校 入学<small className="text-gray-600 dark:text-gray-400 ml-1">(結構楽しいです)</small></p>
       <hr />
       <h2 className="text-xl">京都テックでやりたいこと</h2>
       <ul className="ml-5 list-disc">
         <li>いろんな言語を勉強する</li>
         <li>つよくなる</li>
         <li>おともだちをいっぱいつくる</li>
+        <li>さいきょうのVimmerになる</li>
       </ul>
       <hr />
       <h2 className="text-xl">やったこと</h2>
       <ul className="ml-5 list-disc">
         <li>バイト先でちょっとしたツールつくったよ</li>
-        <li>バイト先のパソコンにLinuxを突っ込んだよ</li>
+        <li>バイト先のパソコンにUbuntuを突っ込んだよ</li>
         <li>
           ノートPCに入ってるWindowsを消し飛ばしてLinux突っ込んだらそれもぶっ壊れたよ
         </li>
+        <li>Adobeからログアウトしてないやつのプロフィールを片っ端からログアウトしろ太郎に変えたよ</li>
       </ul>
     </>
   );
