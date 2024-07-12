c47d912e87d9dcea0775f30c3684ee04e53fe646
Shieru-ktc <167487026+Shieru-ktc@users.noreply.github.com>
2024-07-12 14:08:46 +0900

    ホームをメニューから消したよ
    代わりに、自己紹介のとこ押したらホームに飛ぶようにしたよ

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,9 @@
 import { twMerge } from "tailwind-merge";
 import PropsWithClassName from "../app/utils";
 import HeaderMenu, { HeaderMenuItem } from "./HeaderMenu";
+import Link from "next/link";
 
 const MENU_ITEMS: HeaderMenuItem[] = [
-  {
-    href: "/",
-    children: "ホーム",
-  },
   {
     href: "/profile",
     children: "プロフィール",
@@ -33,11 +30,11 @@ const Header: React.FC<PropsWithClassName> = ({ className }) => {
   return (
     <header
       className={twMerge(
-        "flex h-auto rounded-xl bg-blue-200 px-2 py-4 dark:bg-blue-800 flex-col xl:flex-row",
+        "flex h-auto rounded-xl bg-blue-200 px-2 py-4 dark:bg-blue-800 md:max-xl:flex-col xl:flex-row",
         className,
       )}
     >
-      <span className="my-auto text-2xl font-bold">自己紹介</span>
+      <Link className="my-auto text-2xl font-bold" href="/">自己紹介</Link>
       <div className="ml-auto flex">
         <HeaderMenu items={MENU_ITEMS} />
       </div>
